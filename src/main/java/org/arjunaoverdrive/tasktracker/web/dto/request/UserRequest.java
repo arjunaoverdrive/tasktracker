22b1a4f3b786a9c603c8b3cbaa390ec3cbc47f88
Климов Игорь <igor1984s@yandex.ru>
2024-03-23 17:05:46 +0300

    validate user fields

--- src/main/java/org/arjunaoverdrive/tasktracker/web/dto/request/UserRequest.java
+++ src/main/java/org/arjunaoverdrive/tasktracker/web/dto/request/UserRequest.java
@@ -1,9 +1,13 @@
 package org.arjunaoverdrive.tasktracker.web.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRequest {
+    @NotBlank
     private String username;
+    @Email
     private String email;
 }
